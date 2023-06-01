0ac6ed63e97c4fde563f07639be68ef99cc10cd3
Radeonares32 <bugra.linux.js@gmail.com>
2023-06-01 23:13:44 +0300

    UserRoute getFindByUsernameUserRoute method added

--- backend/src/modules/users/infra/http/routes/UserRoutes.ts
+++ backend/src/modules/users/infra/http/routes/UserRoutes.ts
@@ -6,6 +6,9 @@ import { UserController } from '../controllers/UserController'
 const app = express.Router()
 const userController:UserController = new UserController()
 
-
+//Get
 export const getFindAllUserRoute = app.get('/findAll',userController.getFindAllUser)
 export const getFindByIdUserRoute = app.get('/:id',userController.getFindByIdUser)
+export const getFindByUsernameUserRoute = app.get('/findByUsername',userController.getFindByUsernameUser)
+
+//Post
