68dda855aa9bbdea10dab680d5fb20da4d84b35a
Radeonares32 <bugra.linux.js@gmail.com>
2023-06-01 23:12:05 +0300

    UserRoutes created

---
+++ backend/src/modules/users/infra/http/routes/UserRoutes.ts
@@ -0,0 +1,10 @@
+import express,{Handler} from 'express'
+
+//user controller
+import { UserController } from '../controllers/UserController'
+
+const app = express.Router()
+const userController:UserController = new UserController()
+
+
+export const getFindAllUserRoute = app.get('/findAll',userController.getFindAllUser)
