cd91810e88199f38ed5ee169b4c5607ad8bfda65
Radeonares32 <bugra.linux.js@gmail.com>
2023-06-03 21:52:13 +0300

    sign method created

--- backend/src/modules/users/provider/TokenProvider/implementations/JwtProvider.ts
+++ backend/src/modules/users/provider/TokenProvider/implementations/JwtProvider.ts
@@ -1,22 +1,24 @@
-import { verify,decode,sign, Jwt, JwtPayload } from 'jsonwebtoken'
+import { verify, decode, sign, Jwt, JwtPayload } from "jsonwebtoken";
 
 //jwt repository
-import { ITokenProvider } from '../models/ITokenProvider'
+import { ITokenProvider } from "../models/ITokenProvider";
 
 //config
-import { configs } from '../../../../../config/config'
+import { configs } from "../../../../../config/config";
 
-configs.dotenvConfig()
+configs.dotenvConfig();
 
 export class TokenProvider implements ITokenProvider {
-    generateToken(payload: string): Promise<Jwt> {
-        throw new Error('Method not implemented.')
-    }
-    verifyToken(token: string): Promise<JwtPayload> {
-        throw new Error('Method not implemented.')
-    }
-    decodeToken(token: string): Promise<JwtPayload> {
-        throw new Error('Method not implemented.')
-    }
-
+  generateToken(payload: string): Promise<Jwt> {
+    return new Promise(async (resolve, reject) => {
+      const token: Jwt | any = sign(payload, process.env.SECRET_KEY as string);
+      resolve(token);
+    });
+  }
+  verifyToken(token: string): Promise<JwtPayload> {
+    throw new Error("Method not implemented.");
+  }
+  decodeToken(token: string): Promise<JwtPayload> {
+    throw new Error("Method not implemented.");
+  }
 }
