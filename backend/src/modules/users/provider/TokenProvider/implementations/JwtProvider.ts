92bac4d33f31cc949cb0b0d1762239fb61f1b4f8
Radeonares32 <bugra.linux.js@gmail.com>
2023-06-03 21:54:12 +0300

    verifyToken method created

--- backend/src/modules/users/provider/TokenProvider/implementations/JwtProvider.ts
+++ backend/src/modules/users/provider/TokenProvider/implementations/JwtProvider.ts
@@ -11,12 +11,29 @@ configs.dotenvConfig();
 export class TokenProvider implements ITokenProvider {
   generateToken(payload: string): Promise<Jwt> {
     return new Promise(async (resolve, reject) => {
-      const token: Jwt | any = sign(payload, process.env.SECRET_KEY as string);
-      resolve(token);
+      try {
+        const token: Jwt | any = sign(
+          payload,
+          process.env.SECRET_KEY as string
+        );
+        resolve(token);
+      } catch (err) {
+        reject({ message: err });
+      }
     });
   }
   verifyToken(token: string): Promise<JwtPayload> {
-    throw new Error("Method not implemented.");
+    return new Promise(async (resolve, reject) => {
+      try {
+        const verifyToken: JwtPayload | any = verify(
+          token,
+          process.env.SECRET_KEY as string
+        );
+        resolve(verifyToken);
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
   }
   decodeToken(token: string): Promise<JwtPayload> {
     throw new Error("Method not implemented.");
