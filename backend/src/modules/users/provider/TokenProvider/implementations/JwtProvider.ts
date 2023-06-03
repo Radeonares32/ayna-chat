ca60a821eb45d823b85beab1c3abe9234621bf36
Radeonares32 <bugra.linux.js@gmail.com>
2023-06-03 21:49:54 +0300

    secret keys for dotenv added

--- backend/src/modules/users/provider/TokenProvider/implementations/JwtProvider.ts
+++ backend/src/modules/users/provider/TokenProvider/implementations/JwtProvider.ts
@@ -3,6 +3,11 @@ import { verify,decode,sign, Jwt, JwtPayload } from 'jsonwebtoken'
 //jwt repository
 import { ITokenProvider } from '../models/ITokenProvider'
 
+//config
+import { configs } from '../../../../../config/config'
+
+configs.dotenvConfig()
+
 export class TokenProvider implements ITokenProvider {
     generateToken(payload: string): Promise<Jwt> {
         throw new Error('Method not implemented.')
